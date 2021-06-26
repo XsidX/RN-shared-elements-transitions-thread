e24ea5f69a35b26a08a8957eb4a33a517e8463d6
iamsid <developer.sid@outlook.com>
2021-06-26 17:46:40 +0300

    Setup

---
+++ rnSharedTransitions/.eslintrc.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+// };
+{
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+
+    "env": {
+        "es6": true
+    }
+}
